024bc0307e3e6c1b76c949f5d8ae857277999354
Manas Paranjape <manas@designX6.com>
2016-12-31 18:46:47 -0500

    Getting progress on word list.

--- word-occurences.py
+++ word-occurences.py
@@ -1,12 +1,23 @@
-filename = input('Please type the document you want to result ')
+filename = input('Please type in the document you want to result ')
 def load_content(filename):
     f = open(filename, "r")
     content = f.read()
     f.close()
     return content
 
+def load_dictionary(content):
+    my_list = content.split()
+    strip_duplicates(my_list)
+
+def strip_duplicates(my_list):
+    count = len(my_list)
+    while count > 0:
+        count = count -1
+        word = my_list[count]
+        print (word)
+
 def analyze_content(content):
-    print(content)
+    load_dictionary(content)
     
 content = load_content(filename)
 analyze_content(content)
