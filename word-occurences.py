79851f401464cd5dc66a7a2c484d90580dd48e15
Manas Paranjape <manas@designX6.com>
2017-01-21 18:54:25 -0500

    Introduced lstrip, but having problems.

--- word-occurences.py
+++ word-occurences.py
@@ -1,9 +1,16 @@
+from string import punctuation
 filename = input('Please type in the document you want to result ')
 def load_content(filename):
     f = open(filename, "r")
     content = f.read()
     f.close()
     return content
+
+def strip_punctuation(word):
+    chars = ('nsmnvdjh.,?:;"\'')
+    word.lstrip(chars)
+    print(word)
+    return
 
 def load_dictionary(content):
     my_list = content.split()
@@ -19,26 +26,27 @@ def load_dictionary(content):
             dictionary[word] = occurence
 ##          print("Yes", word, occurence)
         else:
-##          print("No", word)
+            print("No", word)
+##            if word.isalpha is False:
+            strip_punctuation(word)
+                
             dictionary[word] = 1
     return dictionary
-        
-        
+
+    
 def list_words(my_list):
     count = len(my_list)
     while count > 0:
         count = count -1
         word = my_list[count]
-        if word.isalpha() is False:
-            print(word)
-##      print (word)
-        return
+##        if word.isalpha() is False:
+##           print(word)
 
 def analyze_content(content):
     dictionary = load_dictionary(content)
 ##    for k,v in dictionary.items():
 ##        print(k, v)
-##      print(dictionary)   
+##    print(dictionary)   
     
 content = load_content(filename)
 analyze_content(content)
